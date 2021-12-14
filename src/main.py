12cbcf1a41ef3c1ff3690088e3941c13d85f97f7
Amit Verma <vermamit89@gmail.com>
2021-12-14 21:39:36 +0530

    chore: show all users from database

--- src/main.py
+++ src/main.py
@@ -22,13 +22,21 @@ def get_db():
 def start():
     return "server is live"
 
-@app.post('/signin')
+@app.post('/user/signin')
 def new_user(req:models.schemas.User_creation,db: Session=Depends(get_db)):
     new_user=models.models.User(email=req.email,password=req.password,mobile=req.mobile)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get('/user')
+def show_all(db: Session=Depends(get_db)):
+    users=db.query(models.models.User).all()
+    return users
+
+
+
 
 if __name__:"__main__"
 uvicorn.run(app,host='127.0.0.1',port=3000)
