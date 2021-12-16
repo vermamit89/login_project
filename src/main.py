8f9489a53b5ee228f20d5020934b643cc85b2cb8
Amit Verma <vermamit89@gmail.com>
2021-12-16 18:57:32 +0530

    chore:making different routes for server also  .

--- src/main.py
+++ src/main.py
@@ -1,18 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
-import models.models,models.database,routes.user
+import models.models,models.database,routes.user,routes.server
 
 app=FastAPI()
 
 models.models.base.metadata.create_all(models.database.engine)
 
 app.include_router(routes.user.router)
+app.include_router(routes.server.router)
 
-
-
-@app.get('/')
-def start():
-    return "server is live"
 
 if __name__:"__main__"
 uvicorn.run(app,host='127.0.0.1',port=3000)
---
+++ src/routes/server.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+
+router=APIRouter(tags=['Server'])
+
+@router.get('/')
+def start():
+    return "server is live"
