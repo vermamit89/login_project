7fd31779424dfa8172279a426fb9d441d83c5e1f
Amit Verma <vermamit89@gmail.com>
2021-12-18 00:10:38 +0530

    fix: main script

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+import models.models,models.database,routes.user,routes.common,routes.authentication,routes.verify
+
+app=FastAPI()
+
+models.models.base.metadata.create_all(models.database.engine)
+
+app.include_router(routes.common.router)
+app.include_router(routes.authentication.router)
+app.include_router(routes.user.router)
+app.include_router(routes.verify.router)
+
+
+
+if __name__:"__main__"
+uvicorn.run(app,host='127.0.0.1',port=3000)
