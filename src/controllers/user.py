8ff04bb096ac5770d2442b60346cceb1fabdb627
Amit Verma <vermamit89@gmail.com>
2021-12-18 09:58:14 +0530

    fix: error handling

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -13,19 +13,28 @@ def create_user(req:schemas.User_creation,db: Session=Depends(get_db)):
     if user:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                             detail=f'User record already exist!!!')
-    u_id=uuid.uuid4()
-    h_p= bcrypt.hash(req.password)
-    h_m= bcrypt.hash(req.mobile)
-    new_user=models.User(name=req.name,email=req.email,password=h_p,
-                                 mobile=h_m,isVerified=False,uniqueId=str(u_id))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    yag = yagmail.SMTP("vermatest1@gmail.com",'Test@Verma2')
-    contents = ['https://fast-api-userbase.herokuapp.com/verify/' + new_user.uniqueId]
-    yag.send(new_user.email, 'Verify-Email', contents)
+    if len(req.mobile) == 10 and req.mobile.isnumeric():
+        u_id=uuid.uuid4()
+        h_p= bcrypt.hash(req.password)
+        h_m= bcrypt.hash(req.mobile)
+        new_user=models.User(name=req.name,email=req.email,password=h_p,
+                                    mobile=h_m,isVerified=False,uniqueId=str(u_id))
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        yag = yagmail.SMTP("vermatest1@gmail.com",'Test@Verma2')
+        contents = [
+            "Thanks for signing up with us! You must follow this link to verify your account:",
 
-    return new_user
+            "https://fast-api-userbase.herokuapp.com/verify/"+new_user.uniqueId,
+
+            "Have fun, and don't hesitate to contact us with your feedback."
+            ]
+        yag.send(new_user.email, 'Verify-Email', contents)
+
+        return f"We have sent a verification mail to '{new_user.email}'. Please check your inbox and verify to continue."
+    else: 
+        return f"Please provide a valid mobile number!"
 
 
 
